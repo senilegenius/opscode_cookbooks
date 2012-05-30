4ab722c33ed716fc5510d1ab16d5f321c2054866
Nathaniel Eliot <temujin9@t9productions.com>
2012-05-30 18:50:59 -0500

    Making jpackage compatible with CentOS 6.2 (removing unnecessary patching)

--- jpackage/recipes/default.rb
+++ jpackage/recipes/default.rb
@@ -26,30 +26,32 @@ when "redhat","centos","fedora"
     action :install
   end
   
-  execute "yum clean all" do
-    action :nothing
-  end
-  
-  template "/etc/yum.repos.d/jpackage#{node[:jpackage][:version].sub(/\./,'')}.repo" do
-    mode "0644"
-    source "jpackage.repo.erb"
-    notifies :run, resources(:execute => "yum clean all"), :immediately
-  end
-  
-  # fix the jpackage-utils issue
-  # https://bugzilla.redhat.com/show_bug.cgi?id=497213
-  # http://plone.lucidsolutions.co.nz/linux/centos/jpackage-jpackage-utils-compatibility-for-centos-5.x
-  remote_file "#{Chef::Config[:file_cache_path]}/jpackage-utils-compat-el5-0.0.1-1.noarch.rpm" do
-    checksum "c61f2a97e4cda0781635310a6a595e978a2e48e64cf869df7d339f0db6a28093"
-    source "http://plone.lucidsolutions.co.nz/linux/centos/images/jpackage-utils-compat-el5-0.0.1-1.noarch.rpm"
-    mode "0644"
-  end
-  
-  package "jpackage-utils-compat-el5" do
-    source "#{Chef::Config[:file_cache_path]}/jpackage-utils-compat-el5-0.0.1-1.noarch.rpm"
-    options "--nogpgcheck" 
-    version "0.0.1"
-    action :install
+  unless node[:platform_version] == '6.2'
+    execute "yum clean all" do
+      action :nothing
+    end
+    
+    template "/etc/yum.repos.d/jpackage#{node[:jpackage][:version].sub(/\./,'')}.repo" do
+      mode "0644"
+      source "jpackage.repo.erb"
+      notifies :run, resources(:execute => "yum clean all"), :immediately
+    end
+    
+    # fix the jpackage-utils issue
+    # https://bugzilla.redhat.com/show_bug.cgi?id=497213
+    # http://plone.lucidsolutions.co.nz/linux/centos/jpackage-jpackage-utils-compatibility-for-centos-5.x
+    remote_file "#{Chef::Config[:file_cache_path]}/jpackage-utils-compat-el5-0.0.1-1.noarch.rpm" do
+      checksum "c61f2a97e4cda0781635310a6a595e978a2e48e64cf869df7d339f0db6a28093"
+      source "http://plone.lucidsolutions.co.nz/linux/centos/images/jpackage-utils-compat-el5-0.0.1-1.noarch.rpm"
+      mode "0644"
+    end
+    
+    package "jpackage-utils-compat-el5" do
+      source "#{Chef::Config[:file_cache_path]}/jpackage-utils-compat-el5-0.0.1-1.noarch.rpm"
+      options "--nogpgcheck" 
+      version "0.0.1"
+      action :install
+    end
   end
   
   package "jpackage-utils" do
