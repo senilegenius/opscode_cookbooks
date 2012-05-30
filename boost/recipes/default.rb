8b2e9b14bf1958ea16a7632626fef88d95873a1c
Nathaniel Eliot <temujin9@t9productions.com>
2012-05-30 18:47:57 -0500

    Making boost compatible with CentOS

--- boost/recipes/default.rb
+++ boost/recipes/default.rb
@@ -16,4 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-package "libboost-dev"
+package value_for_platform(
+  ["centos","redhat","fedora" ] => {
+    "default" => "boost-devel"
+  },
+  "default" => "libboost-dev"
+)
