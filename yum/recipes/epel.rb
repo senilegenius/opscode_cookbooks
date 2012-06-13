4ae6909374a332f2b8f42ef615391e0c24cc2c1b
Nathaniel Eliot <temujin9@t9productions.com>
2012-06-13 18:01:27 -0500

    Removing broken notifications from yum::epel, in favor of linear execution

--- yum/recipes/epel.rb
+++ yum/recipes/epel.rb
@@ -26,13 +26,11 @@ epel  = node['yum']['epel_release']
 remote_file "#{Chef::Config[:file_cache_path]}/epel-release-#{epel}.noarch.rpm" do
   source "http://download.fedoraproject.org/pub/epel/#{major}/i386/epel-release-#{epel}.noarch.rpm"
   not_if "rpm -qa | egrep -qx 'epel-release-#{epel}(|.noarch)'"
-  notifies :install, "rpm_package[epel-release]", :immediately
 end
 
 rpm_package "epel-release" do
   source "#{Chef::Config[:file_cache_path]}/epel-release-#{epel}.noarch.rpm"
   only_if {::File.exists?("#{Chef::Config[:file_cache_path]}/epel-release-#{epel}.noarch.rpm")}
-  action :nothing
 end
 
 file "epel-release-cleanup" do
