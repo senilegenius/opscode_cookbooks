9db7e00e2e704acb19df77941e5b95da9fa5d79f
Nathaniel Eliot <temujin9@t9productions.com>
2012-06-13 16:41:02 -0500

    Updating Erlang to work on CentOS 6 (using OpenSUSE Scalaris repo)

--- erlang/attributes/default.rb
+++ erlang/attributes/default.rb
@@ -1,1 +1,14 @@
 default[:erlang][:gui_tools] = false
+
+case node[:platform]
+when "redhat", "centos", "scientific"
+  version = node[:platform_version].to_f
+  case 
+  when ( ( version >= 5.0 ) and ( version < 6.0 ) )
+    node[:erlang][:yum_repo] = 'http://repos.fedorapeople.org/repos/peter/erlang/epel-5Server/$basearch'
+  when ( version >= 6.0 )
+    node[:erlang][:yum_repo] = 'http://download.opensuse.org/repositories/home:/scalaris/CentOS_6/'
+  else
+    Chef::Log.warn("No yum repo defined: erlang install may fail")
+  end
+end
