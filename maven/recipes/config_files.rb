9cbf60186e0e14a06d098ad83447c5300a08a676
Josh Bronson <josh.bronson@gmail.com>
2012-07-20 15:53:35 -0500

    added a settings.xml in which to place information about an infochimps repository

---
+++ maven/recipes/config_files.rb
@@ -0,0 +1,8 @@
+template "#{node[:maven][:conf_dir]}/settings.xml" do
+    owner "root"
+    group "root"
+    mode "0644"
+    variables(:aws_access_key        => node[:aws][:aws_access_key],
+              :aws_secret_access_key => node[:aws][:aws_secret_access_key])
+    source "settings.xml.erb"
+end
