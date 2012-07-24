ac334baae8e66066994869c25c6d16d953bbf0f3
Josh Bronson <josh.bronson@gmail.com>
2012-07-24 18:36:26 -0500

    added some bins to our java::oracle recipe

--- java/recipes/oracle.rb
+++ java/recipes/oracle.rb
@@ -58,7 +58,7 @@ java_ark "jdk" do
   url tarball_url
   checksum tarball_checksum
   app_home java_home
-  bin_cmds ["java", "jar"]
+  bin_cmds ["java", "jar", "javac", "javadoc", "jdb"]
   action :install
 end
 
